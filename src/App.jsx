f31e4b4337aad1300eaf1b54391fe2c606744db3
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-08-29 17:54:31 +0300

    feat: ✨ Add basic parser for ctm files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { createEffect, createResource, createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
+import { createResource } from 'solid-js'
 import './App.css'
 
+const CTMFileToRawText = async fileName => {
+  const response = await fetch("./" + fileName);
+  const buffer = await response.arrayBuffer();
+  const decoder = new TextDecoder("ISO-8859-1");
+  return decoder.decode(buffer);
+}
+
+const CTMTextToRawObject = text => {
+  const sections = text.split(/\[(.*)\]/g);
+  const rawObject = {}
+  for (let i = 1; i < sections.length; i += 2) {
+    const header = sections[i];
+    const data = sections[i + 1];
+    rawObject[header] = data.split("\n").map(row => row.split("\t"));
+  }
+
+  return rawObject;
+};
+
+const formatRawCTMObject = rawObject => {
+  // Format configuration
+  // Format Measures
+  // ...
+
+  return rawObject;
+};
 
 function App() {
   const [ctmData] = createResource(async () => {
-    const response = await fetch("./CTM448.CTM");
-    const buffer = await response.arrayBuffer();
-    const decoder = new TextDecoder("ISO-8859-1");
-    return decoder.decode(buffer);
+    const text = await CTMFileToRawText("CTM448.CTM");
+    const object = CTMTextToRawObject(text);
+    console.log(object);
+    return text;
   });
 
   return (
