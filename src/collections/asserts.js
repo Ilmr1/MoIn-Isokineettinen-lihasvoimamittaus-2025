50c620b09b2b4dff1fdc0f7b281b99c87b7aaf41
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-09-02 18:10:04 +0300

    chore: 🧦 Change default asserts.unreachable error text

--- src/collections/asserts.js
+++ src/collections/asserts.js
@@ -6,7 +6,7 @@ export const assertTrue = (condition, message = "Not true") => {
 
 export const assertFalse = (condition, message = "Not false") => assertTrue(!condition, message);
 
-export const unreachable = (message = "Not false") => assertTrue(false, message);
+export const unreachable = (message = "Unreachable code path reached") => assertTrue(false, message);
 
 export const assertTypeArray = (array, message = "Value is array") => assertTrue(Array.isArray(array), message);
 export const assertTypeNumber = (number, message = "Value is number") => assertTrue(typeof number === "number" && !Number.isNaN(number), message);
