744208ed1cf37ee5d0e9838e899bb39a75caa2bd
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-09-11 16:28:28 +0300

    chore: 🧦 Change indentation

--- src/components/FileBrowser.jsx
+++ src/components/FileBrowser.jsx
@@ -3,98 +3,98 @@ import { fileUtils, indexedDBUtils } from "../utils/utils";
 import FilterFilesFromActiveFolders from "../workers/filterFilesFromActiveFolders.js?worker";
 
 export function FileBrowser() {
-    const [files, setFiles] = createSignal([]);
-    const [recentFolders, setRecentFolders] = createSignal([]);
-    const [foldersThatHaveAccess, setFoldersThatHaveAccess] = createSignal([]);
+  const [files, setFiles] = createSignal([]);
+  const [recentFolders, setRecentFolders] = createSignal([]);
+  const [foldersThatHaveAccess, setFoldersThatHaveAccess] = createSignal([]);
 
-    createRenderEffect(on(recentFolders, async folders => {
-        const newFoldersThatHaveAccess = [];
+  createRenderEffect(on(recentFolders, async folders => {
+    const newFoldersThatHaveAccess = [];
 
-        for (const folderHandler of folders) {
-            const access = await fileUtils.checkFileAccess(folderHandler, "readwrite");
-            if (access) {
-                newFoldersThatHaveAccess.push(folderHandler);
-            }
-        }
-
-        setFoldersThatHaveAccess(newFoldersThatHaveAccess);
-    }, { defer: true }));
-
-    createRenderEffect(async () => {
-        const files = await indexedDBUtils.getValue("file-handlers", "recent-files");
-        setRecentFolders(files);
-    });
-
-
-    let worker;
-    const sendToWorker = () => {
-        if (window.Worker) {
-            worker = worker instanceof Worker ? worker : new FilterFilesFromActiveFolders();
-
-            worker.postMessage({
-                activeFolders: foldersThatHaveAccess(),
-            });
-
-            worker.onmessage = async message => {
-                if(message.data === "success") {
-                    const files = await indexedDBUtils.getValue("file-handlers", "filtered-files");
-                    setFiles(files);
-                    console.log("response", files);
-                }
-            }
-        }
+    for (const folderHandler of folders) {
+      const access = await fileUtils.checkFileAccess(folderHandler, "readwrite");
+      if (access) {
+        newFoldersThatHaveAccess.push(folderHandler);
+      }
     }
 
-    createRenderEffect(on(foldersThatHaveAccess, sendToWorker, { defer: true }));
+    setFoldersThatHaveAccess(newFoldersThatHaveAccess);
+  }, { defer: true }));
 
-    const handleOpenDirectory = async () => {
-        const directoryHandler = await window.showDirectoryPicker({ id: "innovation-project", mode: "readwrite" });
-        const folders = await indexedDBUtils.getValue("file-handlers", "recent-files") || [];
+  createRenderEffect(async () => {
+    const files = await indexedDBUtils.getValue("file-handlers", "recent-files");
+    setRecentFolders(files);
+  });
 
-        for (const folder of folders) {
-            if (await folder.isSameEntry(directoryHandler)) {
+
+  let worker;
+  const sendToWorker = () => {
+    if (window.Worker) {
+      worker = worker instanceof Worker ? worker : new FilterFilesFromActiveFolders();
+
+      worker.postMessage({
+        activeFolders: foldersThatHaveAccess(),
+      });
+
+      worker.onmessage = async message => {
+        if(message.data === "success") {
+          const files = await indexedDBUtils.getValue("file-handlers", "filtered-files");
+          setFiles(files);
+          console.log("response", files);
+        }
+      }
+    }
+  }
+
+  createRenderEffect(on(foldersThatHaveAccess, sendToWorker, { defer: true }));
+
+  const handleOpenDirectory = async () => {
+    const directoryHandler = await window.showDirectoryPicker({ id: "innovation-project", mode: "readwrite" });
+    const folders = await indexedDBUtils.getValue("file-handlers", "recent-files") || [];
+
+    for (const folder of folders) {
+      if (await folder.isSameEntry(directoryHandler)) {
+        return;
+      }
+    }
+
+    folders.push(directoryHandler);
+    await await indexedDBUtils.setValue("file-handlers", "recent-files", folders);
+
+    setRecentFolders(recentFolders);
+  }
+
+  return (
+    <div>
+      <button onClick={handleOpenDirectory}>Open Folder</button>
+      <ul>
+        <For each={recentFolders()}>{(directoryHandler, i) => (
+          <li>
+            <span>{directoryHandler.name} </span>
+            <button onClick={async () => {
+              const access = await fileUtils.checkOrGrantFileAccess(directoryHandler, "readwrite");
+              if (!access) {
                 return;
-            }
-        }
+              }
 
-        folders.push(directoryHandler);
-        await await indexedDBUtils.setValue("file-handlers", "recent-files", folders);
+              setFoldersThatHaveAccess(folders => [...folders, directoryHandler]);
+            }}>load</button>
+            <button onClick={async () => {
+              const files = await indexedDBUtils.mutateValue("file-handlers", "recent-files", result => {
+                const recentFiles = result || [];
+                recentFiles.splice(i(), 1);
+                return recentFiles;
+              });
 
-        setRecentFolders(recentFolders);
-    }
-
-    return (
-        <div>
-            <button onClick={handleOpenDirectory}>Open Folder</button>
-            <ul>
-                <For each={recentFolders()}>{(directoryHandler, i) => (
-                    <li>
-                        <span>{directoryHandler.name} </span>
-                        <button onClick={async () => {
-                            const access = await fileUtils.checkOrGrantFileAccess(directoryHandler, "readwrite");
-                            if (!access) {
-                                return;
-                            }
-
-                            setFoldersThatHaveAccess(folders => [...folders, directoryHandler]);
-                        }}>load</button>
-                        <button onClick={async () => {
-                            const files = await indexedDBUtils.mutateValue("file-handlers", "recent-files", result => {
-                                const recentFiles = result || [];
-                                recentFiles.splice(i(), 1);
-                                return recentFiles;
-                            });
-
-                            setRecentFolders(files);
-                        }}>delete</button>
-                    </li>
-                )}</For>
-                {files().map((file) => (
-                    <li>
-                        <p>{file.name} {file.measurementDate} {file.subjectFirstName} {file.subjectLastName}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+              setRecentFolders(files);
+            }}>delete</button>
+          </li>
+        )}</For>
+        {files().map((file) => (
+          <li>
+            <p>{file.name} {file.measurementDate} {file.subjectFirstName} {file.subjectLastName}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
