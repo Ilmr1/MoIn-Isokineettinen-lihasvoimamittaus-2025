32175ba9f71eb2dffd58eca7992ed4024a966fdd
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-09-27 21:56:17 +0300

    refactor: 🧩 Move repetitions to it's own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,24 @@
-import { createResource, createSignal, For } from 'solid-js'
+import { createSignal, For } from 'solid-js'
 import './App.css'
-import { CTMUtils, fileUtils, indexedDBUtils, numberUtils } from './utils/utils';
-import { GenericSVGChart } from './components/GenericSVGChart.jsx';
+import { fileUtils } from './utils/utils';
 import { ThreeCharts } from './components/ThreeCharts.jsx';
 import { FileBrowser } from './components/FileBrowser.jsx';
 import { parsedFileContext } from './providers.js';
 import { AverageChart } from './components/AverageChart.jsx';
-import { Header } from './components/Header.jsx';
 import { Sidebar } from './components/Sidebar.jsx';
-//import { Files } from "./components/FilesPage"
+import { Repetitions } from './components/Repetitions.jsx';
 
 function App() {
-  const [fileName, setFileName] = createSignal("CTM450.CTM");
   const [parsedFileData, setParsedFileData] = createSignal([]);
 
-  const [ctmData] = createResource(fileName, async name => {
-    const text = await fileUtils.fetchCTMFileWithName(name);
-    const formatted = CTMUtils.parseTextToObject(text);
+  const saveDataAsCSV = (data) => {
+    const columns = ["Kammen voima", "Kammen nopeus", "Kammen kulma"];
+    fileUtils.generateFileAndDownload(fileUtils.formatToCSV(data, columns), "data.csv", "csv");
+  }
 
-    console.log(formatted);
-
-    return { text, formatted };
-  });
-
-  indexedDBUtils.openStore("file-handlers", "readwrite");
+  const printDataAsTextToConsole = (data) => {
+    console.log(data.map(row => row.map(val => val.toFixed(3)).join("\t")).join("\n"));
+  }
 
   return (
     <parsedFileContext.Provider value={{ parsedFileData, setParsedFileData }}>
@@ -37,43 +32,13 @@ function App() {
             <For each={parsedFileData()}>{parsedData => (
               <>
                 <ThreeCharts parsedCTM={parsedData.rawObject} />
-                <br />
-                {/* <GenericSVGChart title="Power" parsedCTM={ctmData().formatted} dataIndex={0} min={ctmData().formatted.minmax.minPower} max={ctmData().formatted.minmax.maxPower} /><br /> */}
-                {/* <GenericSVGChart title="Speed" parsedCTM={ctmData().formatted} dataIndex={1} min={ctmData().formatted.minmax.minSpeed} max={ctmData().formatted.minmax.maxSpeed} /><br /> */}
-                {/* <GenericSVGChart title="Angle" parsedCTM={ctmData().formatted} dataIndex={2} min={ctmData().formatted.minmax.minAngle} max={ctmData().formatted.minmax.maxAngle} /><br /> */}
-                <button
-
-                onClick={() => fileUtils.generateFileAndDownload(fileUtils.formatToCSV(ctmData().formatted.data, ["Kammen voima", "Kammen nopeus", "Kammen kulma"]), "data.csv", "csv")}>CSV</button>
-                <button
-                class="button_blue"
-                onClick={() => console.log(ctmData().formatted.data.map(row => row.map(val => val.toFixed(3)).join("\t")).join("\n"))}>txt</button>
-                <br />
-                <ul>
-                  <For each={Object.entries(parsedData.rawObject.repetitions).sort(([a], [b]) => a.localeCompare(b))}>{([key, values]) => (
-                    <li>{key}:
-                      <For each={values}>{value => (
-                        <span class="mx-2">
-                          <Switch fallback={numberUtils.truncDecimals(value, 2)}>
-                            <Match when={key.startsWith("torquePeakPos")}>{numberUtils.truncDecimals(value, 2)}° </Match>
-                            <Match when={key.startsWith("torquePeak")}>{numberUtils.truncDecimals(value, 2)}Nm </Match>
-                            <Match when={key.startsWith("powerAvg")}>{numberUtils.truncDecimals(value, 2)}W </Match>
-                            <Match when={key.startsWith("powerPeak")}>{numberUtils.truncDecimals(value, 2)}W </Match>
-                            <Match when={key.startsWith("power")}>{numberUtils.truncDecimals(value, 2)} rad/s </Match>
-                            <Match when={key.startsWith("work")}>{numberUtils.truncDecimals(value, 2)}J </Match>
-                            <Match when={key.startsWith("speedPeakPos")}>{numberUtils.truncDecimals(value, 2)}° </Match>
-                            <Match when={key.startsWith("speed")}>{numberUtils.truncDecimals(value, 2)} deg/s </Match>
-                          </Switch>
-                        </span>
-                      )}</For>
-                    </li>
-                  )}</For>
-                </ul>
+                <div>
+                  <button onClick={() => saveDataAsCSV(parsedData.rawObject.data)}>CSV</button>
+                  <button class="button_blue" onClick={() => printDataAsTextToConsole(parsedData.rawObject.data)}>txt</button>
+                </div>
+                <Repetitions repetitions={parsedData.rawObject.repetitions} />
               </>
             )}</For>
-
-            {/* <pre> */}
-            {/*   <code>{ctmData()?.text}</code> */}
-            {/* </pre> */}
           </main>
         </div>
       </div>
