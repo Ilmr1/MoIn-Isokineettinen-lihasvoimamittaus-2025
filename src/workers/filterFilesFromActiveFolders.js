9573d96d53921f99ef2b62f9515f8ab27cfc1dfc
Atte Ristimäki <111856713+Arkkaaa@users.noreply.github.com>
2025-09-30 13:36:51 +0300

    refactor: 🧩 move parsing functionality

--- src/workers/filterFilesFromActiveFolders.js
+++ src/workers/filterFilesFromActiveFolders.js
@@ -8,41 +8,13 @@ onmessage = async (message) => {
 
   console.log("worker", message);
   const filteredFiles = [];
-  const sessionMap = {};
-  let sessionCounter = 0;
 
   for (const folderHandler of activeFolders) {
     for await (const fileHandler of getFilesRecursively(folderHandler)) {
       if (fileHandler.name.endsWith(".CTM") || fileHandler.name.endsWith(".cxp")) {
         const file = await fileHandler.getFile();
         const text = await file.text();
-        const parsedFile = parseCTMForFiltering(text);
-        const measurement = parsedFile.Measurement;
-        const session = parsedFile.session;
-        const date = measurement["date (dd/mm/yyyy)"];
-        const subjectFirstName = session["subject name first"];
-        const subjectLastName = session["subject name"];
-        const sessionKey = date + subjectFirstName + subjectLastName;
-        if (!sessionMap[sessionKey]) {
-          sessionCounter += 1;
-          sessionMap[sessionKey] = sessionCounter;
-        }
-
-        const sessionNumber = sessionMap[sessionKey];
-        const sessionId = `Session ${sessionNumber}`
-        filteredFiles.push({
-          fileHandler,
-          name: fileHandler.name.replace(/\.CTM$/i, ""),
-          measurementType: measurement.name,
-          date,
-          time: measurement["time (hh/mm/ss)"],
-          subjectFirstName,
-          subjectLastName,
-          sessionId,
-          legSide: parsedFile.legSide,
-          program: parsedFile.program,
-          measurementSpeed: parsedFile.measurementSpeed
-        });
+        filteredFiles.push(parseCTMForFiltering(text, fileHandler));
       }
     }
   }
@@ -80,33 +52,56 @@ async function* getFilesRecursively(entry) {
   }
 }
 
-function parseCTMForFiltering(text) {
-  
+function parseCTMForFiltering(text, fileHandler) {
+  if (!parseCTMForFiltering.sessionMap) parseCTMForFiltering.sessionMap = {};
+  if (!parseCTMForFiltering.sessionCounter) parseCTMForFiltering.sessionCounter = 0;
+
   const sections = text.split(/\[(.*)\]/g);
-  const filteredObject = {}
+  const parsedFile = {}
 
   for (let i = 1; i < sections.length; i += 2) {
     const header = sections[i];
     const data = sections[i + 1];
+
     if (header === "Configuration"){
-      /* const sideIndex = data.indexOf("side");
-      const newLineIndex = data.indexOf("\n", sideIndex);
-      let side = data.substring(sideIndex, newLineIndex).split("\t").at(-1); */
-      let side = extractDataFromString(data, "side");
-      let speed = extractDataFromString(data, "speed");
-      let program = extractDataFromString(data, "program");
-      filteredObject.program = program;
-      filteredObject.legSide = side;
-      filteredObject.measurementSpeed = speed;
-      
+      parsedFile.legSide = extractDataFromString(data, "side");
+      parsedFile.speed = extractDataFromString(data, "speed");
+      parsedFile.program = extractDataFromString(data, "program");
     }
+
     if (header === "Measurement" || header === "session"){
       const rows = data.replaceAll("\r", "").trim().split("\n").map(row => row.trim().split("\t"));
-      filteredObject[header] = Object.fromEntries(rows.filter(row => row.length > 1))
+      parsedFile[header] = Object.fromEntries(rows.filter(row => row.length > 1))
     }
   }
-  console.log("asdad",filteredObject)
-  return filteredObject;
+  
+  const { Measurement: measurement, session } = parsedFile;
+  const date = measurement["date (dd/mm/yyyy)"];
+  const subjectFirstName = session["subject name first"];
+  const subjectLastName = session["subject name"];
+  const sessionKey = date + subjectFirstName + subjectLastName;
+
+  if (!parseCTMForFiltering.sessionMap[sessionKey]) {
+    parseCTMForFiltering.sessionCounter += 1;
+    parseCTMForFiltering.sessionMap[sessionKey] = parseCTMForFiltering.sessionCounter;
+  }
+
+  const sessionNumber = parseCTMForFiltering.sessionMap[sessionKey];
+  const sessionId = `Session ${sessionNumber}`;
+
+  return {
+    fileHandler,
+    name: fileHandler.name.replace(/\.CTM$/i, ""),
+    measurementType: measurement.name,
+    date,
+    time: measurement["time (hh/mm/ss)"],
+    subjectFirstName,
+    subjectLastName,
+    sessionId,
+    legSide: parsedFile.legSide,
+    program: parsedFile.program,
+    measurementSpeed: parsedFile.measurementSpeed
+  };
 }
 
 const extractDataFromString = (data, key) => {
