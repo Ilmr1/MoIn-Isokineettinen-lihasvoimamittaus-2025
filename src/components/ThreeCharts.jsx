20762e67929feef78f108ea88e0ead3eda142784
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-10-30 18:10:03 +0200

    rework: 🛠️ Change old charts to new labels charts

--- src/components/ThreeCharts.jsx
+++ src/components/ThreeCharts.jsx
@@ -1,5 +1,5 @@
 import { batch, createSignal, ErrorBoundary, mergeProps, splitProps } from "solid-js";
-import { ChartBorder, ChartGrid, ChartHeader, ChartHorizontalSplitLineWithLabel, ChartMousePositionInPercentage, ChartPadding, ChartPath, ChartVecticalLinePercentageToRelativeIndex, ChartWrapper } from "./GenericSVGChart.jsx";
+import { ChartBorder, ChartGrid, ChartHeader, ChartHorizontalLineFromValue, ChartHorizontalPointLineWithLabel, ChartHorizontalSplitLineWithLabel, ChartHorizontalZeroLine, ChartMousePositionInPercentage, ChartPadding, ChartPath, ChartPercentageVerticalLine, ChartTextTop, ChartVecticalLinePercentageToRelativeIndex, ChartXAxisFloor, ChartYAxisFloor } from "./GenericSVGChart.jsx";
 import "./GenericSVGChart.css";
 import { asserts } from "../collections/collections.js";
 export function ThreeCharts(props) {
@@ -15,25 +15,12 @@ function Chart(props) {
   const [mouseX, setMouseX] = createSignal(-1);
   const [mouseY, setMouseY] = createSignal(-1);
 
-  const width = 800;
-  const height = 200;
-
-  const paddingLeft = 100;
-  const paddingRight = 10;
-  const paddingTop = 18;
-  const paddingBottom = 10;
+  const svgArea = { width: 800, height: 200, x: 0, y: 0 };
 
   const updateHoverCoords = e => batch(() => {
     setMouseX(e.offsetX);
     setMouseY(e.offsetY);
   });
-
-  const sizes = mergeProps({
-    width: width - paddingLeft - paddingRight,
-    height: height - paddingTop - paddingBottom,
-    x: paddingLeft,
-    y: paddingTop,
-  }, props);
 
   const controls = mergeProps({ mouseX, mouseY }, props);
 
@@ -44,51 +31,98 @@ function Chart(props) {
 
   return (
     <>
-      <svg class="cp-chart" width={width} height={height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
-        <ChartWrapper
-          title="Torque"
-          points={props.parsedCTM.pointCollections.power.points}
-          maxValue={props.parsedCTM.pointCollections.power.maxValue}
-          minValue={props.parsedCTM.pointCollections.power.minValue}
-          splits={props.parsedCTM.splitCollections.power.splits}
-          startIndex={props.parsedCTM.splitCollections.power.startIndex}
-          endIndex={props.parsedCTM.splitCollections.power.endIndex}
-          {...sizes}
-          {...controls}
-        />
-      </svg>
-      <svg class="cp-chart" width={width} height={height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
-        <ChartWrapper
-          title="Speed"
-          points={props.parsedCTM.pointCollections.speed.points}
-          maxValue={props.parsedCTM.pointCollections.speed.maxValue}
-          minValue={props.parsedCTM.pointCollections.speed.minValue}
-          splits={props.parsedCTM.splitCollections.speed.splits}
-          startIndex={props.parsedCTM.splitCollections.speed.startIndex}
-          endIndex={props.parsedCTM.splitCollections.speed.endIndex}
-          {...sizes}
-          {...controls}
-        />
-      </svg>
-      <svg class="cp-chart" width={width} height={height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
-        <ChartWrapper
-          title="Angle"
-          points={props.parsedCTM.pointCollections.angle.points}
-          maxValue={props.parsedCTM.pointCollections.angle.maxValue}
-          minValue={props.parsedCTM.pointCollections.angle.minValue}
-          splits={props.parsedCTM.splitCollections.angle.splits}
-          startIndex={props.parsedCTM.splitCollections.angle.startIndex}
-          endIndex={props.parsedCTM.splitCollections.angle.endIndex}
-          {...sizes}
-          {...controls}
-        />
-      </svg>
-      <Test {...props} {...controls} />
+      <LineChartWithLabels
+        {...props}
+        title="Torque"
+        points={props.parsedCTM.pointCollections.power.points}
+        maxValue={props.parsedCTM.pointCollections.power.maxValue}
+        minValue={props.parsedCTM.pointCollections.power.minValue}
+        splits={props.parsedCTM.splitCollections.power.splits}
+        startIndex={props.parsedCTM.splitCollections.power.startIndex}
+        endIndex={props.parsedCTM.splitCollections.power.endIndex}
+      />
+      <LineChartWithLabels
+        {...props}
+        title="Speed"
+        points={props.parsedCTM.pointCollections.speed.points}
+        maxValue={props.parsedCTM.pointCollections.speed.maxValue}
+        minValue={props.parsedCTM.pointCollections.speed.minValue}
+        splits={props.parsedCTM.splitCollections.speed.splits}
+        startIndex={props.parsedCTM.splitCollections.speed.startIndex}
+        endIndex={props.parsedCTM.splitCollections.speed.endIndex}
+      />
+      <LineChartWithLabels
+        {...props}
+        title="Angle"
+        points={props.parsedCTM.pointCollections.angle.points}
+        maxValue={props.parsedCTM.pointCollections.angle.maxValue}
+        minValue={props.parsedCTM.pointCollections.angle.minValue}
+        splits={props.parsedCTM.splitCollections.angle.splits}
+        startIndex={props.parsedCTM.splitCollections.angle.startIndex}
+        endIndex={props.parsedCTM.splitCollections.angle.endIndex}
+      />
+      <CircleBar {...props} {...controls} />
     </>
   );
+
+  function LineChartWithLabels(props) {
+    return (
+      <svg width={svgArea.width} height={svgArea.height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
+        {console.log(props.title, props.minValue , props.maxValue)}
+        <ChartPadding name="border" {...svgArea} paddingLeft={80} paddingRight={50} paddingBottom={22} paddingTop={22}>{borderArea => (
+          <>
+            <ChartTextTop {...borderArea} title={props.title} />
+            <ChartBorder {...borderArea} height={borderArea.height} />
+            <ChartGrid {...borderArea} height={borderArea.height} />
+            <ChartPadding name="lines" {...borderArea} padding={15}>{lineArea => (
+              <>
+                <ChartHorizontalZeroLine
+                  {...lineArea}
+                  x={borderArea.x}
+                  width={borderArea.width}
+                  maxValue={props.maxValue}
+                  minValue={props.minValue}
+                />
+                <ChartXAxisFloor {...borderArea} startValue={props.startIndex / 256} endValue={props.endIndex / 256} x={lineArea.x} width={lineArea.width} />
+                <ChartYAxisFloor {...borderArea} startValue={props.maxValue} endValue={props.minValue} y={lineArea.y} height={lineArea.height} />
+                <g data-line>
+                  <ChartPath
+                    points={props.points}
+                    maxValue={props.maxValue}
+                    minValue={props.minValue}
+                    splits={props.splits}
+                    startIndex={props.startIndex}
+                    endIndex={props.endIndex}
+                    {...lineArea}
+                    {...controls}
+                  ></ChartPath>
+                </g>
+                <ChartMousePositionInPercentage {...controls} {...lineArea}>{mouseArea => (
+                  <>
+                    <ChartPercentageVerticalLine
+                      {...mouseArea}
+                      y={borderArea.y}
+                      height={borderArea.height}
+                    />
+                    <ChartHorizontalPointLineWithLabel
+                      {...props}
+                      {...controls}
+                      {...mouseArea}
+                      x={borderArea.x}
+                      width={borderArea.width}
+                    />
+                  </>
+                )}</ChartMousePositionInPercentage>
+              </>
+            )}</ChartPadding>
+          </>
+        )}</ChartPadding>
+      </svg>
+    )
+  }
 }
 
-function Test(props) {
+function CircleBar(props) {
   asserts.assertTruthy(props.parsedCTM, "parsedCTM");
 
   const svgArea = { width: 800, height: 200, x: 0, y: 0 };
@@ -146,3 +180,4 @@ function Test(props) {
     </svg>
   )
 }
+
