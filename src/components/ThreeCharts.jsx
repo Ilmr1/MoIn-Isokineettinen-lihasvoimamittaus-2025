88f57bbe9688c2a9773f2717dc86e8c272c58b56
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-10-30 19:28:34 +0200

    rework: 🛠️ Add labels to circle chart

--- src/components/ThreeCharts.jsx
+++ src/components/ThreeCharts.jsx
@@ -1,5 +1,5 @@
 import { batch, createSignal, ErrorBoundary, mergeProps, splitProps } from "solid-js";
-import { ChartBorder, ChartGrid, ChartHeader, ChartHorizontalLineFromValue, ChartHorizontalPointLineWithLabel, ChartHorizontalSplitLineWithLabel, ChartHorizontalZeroLine, ChartMousePositionInPercentage, ChartPadding, ChartPath, ChartPercentageVerticalLine, ChartTextTop, ChartVecticalLinePercentageToRelativeIndex, ChartXAxisFloor, ChartYAxisFloor } from "./GenericSVGChart.jsx";
+import { ChartBorder, ChartGrid, ChartHorizontalPointLineWithLabel, ChartHorizontalSplitLineWithLabel, ChartHorizontalZeroLine, ChartMousePositionInPercentage, ChartPadding, ChartPath, ChartPercentageVerticalLine, ChartTextTop, ChartVecticalLinePercentageToRelativeIndex, ChartXAxisFloor, ChartYAxisFloor } from "./GenericSVGChart.jsx";
 import "./GenericSVGChart.css";
 import { asserts } from "../collections/collections.js";
 export function ThreeCharts(props) {
@@ -35,7 +35,7 @@ function Chart(props) {
         <ChartPadding name="lines" {...borderArea} padding={15}>{lineArea => (
           <ChartMousePositionInPercentage {...controls} {...lineArea} y={borderArea.y} height={borderArea.height}>{mouseArea => (
             <>
-              <LineChartWithLabels2
+              <LineChartWithLabels
                 {...props}
                 borderArea={borderArea}
                 lineArea={lineArea}
@@ -48,7 +48,7 @@ function Chart(props) {
                 startIndex={props.parsedCTM.splitCollections.power.startIndex}
                 endIndex={props.parsedCTM.splitCollections.power.endIndex}
               />
-              <LineChartWithLabels2
+              <LineChartWithLabels
                 {...props}
                 borderArea={borderArea}
                 lineArea={lineArea}
@@ -61,7 +61,7 @@ function Chart(props) {
                 startIndex={props.parsedCTM.splitCollections.speed.startIndex}
                 endIndex={props.parsedCTM.splitCollections.speed.endIndex}
               />
-              <LineChartWithLabels2
+              <LineChartWithLabels
                 {...props}
                 borderArea={borderArea}
                 lineArea={lineArea}
@@ -74,7 +74,17 @@ function Chart(props) {
                 startIndex={props.parsedCTM.splitCollections.angle.startIndex}
                 endIndex={props.parsedCTM.splitCollections.angle.endIndex}
               />
-              <CircleBar {...props} {...controls} mouseArea={mouseArea} />
+              <CircleBar
+                {...props}
+                {...controls}
+                mouseArea={mouseArea}
+                points={props.parsedCTM.pointCollections.power.points}
+                splits={props.parsedCTM.splitCollections.power.splits}
+                maxValue={props.parsedCTM.pointCollections.power.maxValue}
+                minValue={props.parsedCTM.pointCollections.power.minValue}
+                startIndex={props.parsedCTM.splitCollections.power.startIndex}
+                endIndex={props.parsedCTM.splitCollections.power.endIndex}
+              />
             </>
           )}</ChartMousePositionInPercentage>
         )}</ChartPadding>
@@ -83,64 +93,6 @@ function Chart(props) {
   );
 
   function LineChartWithLabels(props) {
-    return (
-      <svg width={svgArea.width} height={svgArea.height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
-        <ChartPadding name="border" {...svgArea} paddingLeft={80} paddingRight={50} paddingBottom={22} paddingTop={22}>{borderArea => (
-          <>
-            <ChartTextTop {...borderArea} title={props.title} />
-            <ChartBorder {...borderArea} height={borderArea.height} />
-            <ChartGrid {...borderArea} height={borderArea.height} />
-            <ChartPadding name="lines" {...borderArea} padding={15}>{lineArea => (
-              <>
-                <ChartHorizontalZeroLine
-                  {...lineArea}
-                  x={borderArea.x}
-                  width={borderArea.width}
-                  maxValue={props.maxValue}
-                  minValue={props.minValue}
-                />
-                <ChartXAxisFloor {...borderArea} startValue={props.startIndex / 256} endValue={props.endIndex / 256} x={lineArea.x} width={lineArea.width} />
-                <ChartYAxisFloor {...borderArea} startValue={props.maxValue} endValue={props.minValue} y={lineArea.y} height={lineArea.height} />
-                <g data-line>
-                  <ChartPath
-                    points={props.points}
-                    maxValue={props.maxValue}
-                    minValue={props.minValue}
-                    splits={props.splits}
-                    startIndex={props.startIndex}
-                    endIndex={props.endIndex}
-                    {...lineArea}
-                    {...controls}
-                  ></ChartPath>
-                </g>
-                <ChartMousePositionInPercentage {...controls} {...lineArea} y={borderArea.y} height={borderArea.height}>{mouseArea => (
-                  <>
-                    <ChartPercentageVerticalLine
-                      {...mouseArea}
-                      y={borderArea.y}
-                      height={borderArea.height}
-                    />
-                    <ChartHorizontalPointLineWithLabel
-                      {...props}
-                      {...controls}
-                      {...mouseArea}
-                      {...lineArea}
-                      x={borderArea.x}
-                      width={borderArea.width}
-                    />
-                  </>
-                )}</ChartMousePositionInPercentage>
-              </>
-            )}</ChartPadding>
-          </>
-        )}</ChartPadding>
-      </svg>
-    )
-  }
-  function LineChartWithLabels2(props) {
-    // const props.borderArea = 5;
-    // const props.lineArea = 5;
-    // const props.mouseArea = 5;
     return (
       <svg width={svgArea.width} height={svgArea.height} onMouseLeave={clearHoverCoors} onMouseMove={updateHoverCoords}>
         <ChartTextTop {...props.borderArea} title={props.title} />
@@ -188,57 +140,69 @@ function Chart(props) {
 function CircleBar(props) {
   asserts.assertTruthy(props.parsedCTM, "parsedCTM");
 
-  const svgArea = { width: 500, height: 200, x: 0, y: 0 };
-  const [controls] = splitProps(props, ["mouseX", "mouseY"]);
+  const svgArea = { width: 500, height: 250, x: 0, y: 0 };
 
   return (
-    <svg width={svgArea.width} height={svgArea.height}>
-      <ChartPadding {...svgArea} paddingLeft={100} paddingTop={18} paddingRight={10} paddingBottom={1}>{chartArea => (
-        <>
-          <ChartGrid {...chartArea} />
-          <ChartBorder {...chartArea} />
-          <ChartHeader {...chartArea} title="Torque repetitions" />
-          <ChartPadding {...chartArea} paddingInline={25} paddingBlock={25}>{linesArea => (
-            <For each={props.parsedCTM.splitCollections.power.splits}>{split => (
-              <Show when={!split.disabled}>
-                <ChartPath
-                  points={props.parsedCTM.pointCollections.power.points}
-                  maxValue={props.parsedCTM.pointCollections.power.maxValue}
-                  minValue={props.parsedCTM.pointCollections.power.minValue}
-                  splits={[split]}
-                  flipped={split.color === "blue"}
-                  startIndex={split.startIndex}
-                  endIndex={split.endIndex}
-                  {...linesArea}
-                  {...controls}
-                ></ChartPath>
-                <ChartHorizontalSplitLineWithLabel
-                  points={props.parsedCTM.pointCollections.power.points}
-                  maxValue={props.parsedCTM.pointCollections.power.maxValue}
-                  minValue={props.parsedCTM.pointCollections.power.minValue}
-                  split={split}
-                  startIndex={props.parsedCTM.splitCollections.power.startIndex}
-                  endIndex={props.parsedCTM.splitCollections.power.endIndex}
-                  {...props.mouseArea}
-                  {...linesArea}
-                  x={chartArea.x}
-                  width={chartArea.width}
+    <ErrorBoundary fallback="Circle chart error">
+      <svg width={svgArea.width} height={svgArea.height}>
+        <ChartPadding name="border" {...svgArea} paddingLeft={80} paddingRight={50} paddingBottom={22} paddingTop={22}>{borderArea => (
+          <>
+            <ChartGrid {...borderArea} />
+            <ChartBorder {...borderArea} />
+            <ChartTextTop {...borderArea} title="Torque repetitions" />
+            <ChartPadding name="lines" {...borderArea} padding={15}>{lineArea => (
+              <>
+                <ChartHorizontalZeroLine
+                  {...lineArea}
+                  x={borderArea.x}
+                  width={borderArea.width}
+                  maxValue={props.maxValue}
+                  minValue={props.minValue}
                 />
-                <ChartVecticalLinePercentageToRelativeIndex
-                  points={props.parsedCTM.pointCollections.power.points}
-                  split={split}
-                  flipped={split.color === "blue"}
-                  startIndex={props.parsedCTM.splitCollections.power.startIndex}
-                  endIndex={props.parsedCTM.splitCollections.power.endIndex}
-                  {...props.mouseArea}
-                  {...linesArea}
-                />
-              </Show>
-            )}</For>
-          )}</ChartPadding>
-        </>
-      )}</ChartPadding>
-    </svg>
+                <ChartYAxisFloor {...borderArea} startValue={props.maxValue} endValue={props.minValue} y={lineArea.y} height={lineArea.height} />
+                <For each={props.splits}>{split => (
+                  <Show when={!split.disabled}>
+                    <ChartPath
+                      points={props.points}
+                      maxValue={props.maxValue}
+                      minValue={props.minValue}
+                      splits={[split]}
+                      flipped={split.color === "blue"}
+                      startIndex={split.startIndex}
+                      endIndex={split.endIndex}
+                      {...lineArea}
+                    ></ChartPath>
+                    <ChartHorizontalSplitLineWithLabel
+                      points={props.points}
+                      maxValue={props.maxValue}
+                      minValue={props.minValue}
+                      split={split}
+                      startIndex={props.startIndex}
+                      endIndex={props.endIndex}
+                      {...props.mouseArea}
+                      {...lineArea}
+                      x={borderArea.x}
+                      width={borderArea.width}
+                    />
+                    <ChartVecticalLinePercentageToRelativeIndex
+                      points={props.parsedCTM.pointCollections.power.points}
+                      split={split}
+                      flipped={split.color === "blue"}
+                      startIndex={props.startIndex}
+                      endIndex={props.endIndex}
+                      {...props.mouseArea}
+                      {...lineArea}
+                      y={borderArea.y}
+                      height={borderArea.height}
+                    />
+                  </Show>
+                )}</For>
+              </>
+            )}</ChartPadding>
+          </>
+        )}</ChartPadding>
+      </svg>
+    </ErrorBoundary>
   )
 }
 
