6477e644e159b0b6b931a2ef0fb97a42c1018e8b
kassu11 <61124141+kassu11@users.noreply.github.com>
2025-11-04 18:58:58 +0200

    rework: 🛠️ Add active files and repetitions to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,15 @@
 import {FiPrinter, FiLogOut, FiHardDrive } from 'solid-icons/fi'
 import { BiRegularExport } from 'solid-icons/bi';
-import { For } from "solid-js";
+import { batch, createEffect, createMemo, createSignal, For, untrack } from "solid-js";
 import { generatePDF } from '../utils/pdfUtils';
 import {ListOfSelectedFiles} from "./ListOfSelectedFiles.jsx";
+import { ActiveProgramTypeButtons } from './ActiveProgramTypeButtons.jsx';
+import { $selectedSessionsCounts, activeProgram, selectedFiles, setSelectedFiles, storeSelectedSessionsCounts } from '../signals.js';
+import { useGlobalContext } from '../providers.js';
+import { Button } from './ui/Button.jsx';
+import { ListOfFileHandlerRepetitions } from './ListOfFileHandlerRepetitions.jsx';
+import { produce, reconcile } from 'solid-js/store';
+import { signals } from '../collections/collections.js';
 
 export function Sidebar() {
   const otherTools = [
@@ -29,7 +36,79 @@ export function Sidebar() {
           </div>
         )}
       </For>
-      <ListOfSelectedFiles />
+      <ActiveProgramTypeButtons />
+      <ActiveFilesAndRepetitions />
+      {/* <ListOfSelectedFiles /> */}
     </nav>
+  );
+}
+
+function ActiveFilesAndRepetitions() {
+  const {activeFiles} = useGlobalContext();
+
+  const clearSelectedFiles = () => {
+    batch(() => {
+      setSelectedFiles([]);
+      storeSelectedSessionsCounts(reconcile({}));
+    });
+  }
+
+  const removeFileSelection = (i) => batch(() => {
+    const {fileHandler} = untrack(selectedFiles)[i];
+    batch(() => {
+      setSelectedFiles(files => {
+        files.splice(i, 1);
+        return [...files];
+      });
+      for (const key in $selectedSessionsCounts) {
+        if ($selectedSessionsCounts[key].includes(fileHandler)) {
+          storeSelectedSessionsCounts(produce(store => {
+            const newFiles = store[key].filter(f => f.fileHandler !== fileHandler)
+            if (newFiles.length) {
+              store[key] = newFiles;
+            } else {
+              delete store[key];
+            }
+          }));
+
+          break;
+        }
+      }
+    })
+  });
+
+  const [activeFileIndex, setActiveFileIndex] = createSignal(0);
+
+  createEffect(() => {
+    activeProgram();
+    setActiveFileIndex(0);
+  });
+
+  const activeFile = createMemo(() => activeFiles()[activeFileIndex()]);
+
+  return (
+    <Show when={activeFiles().length}>
+      <div class="flex gap-4">
+        <For each={activeFiles()}>{(fileHandler, i) => (
+          <div class="file-tab-wrapper flex gap-1" classList={{active: activeFileIndex() === i()}}>
+            <button onClick={() => setActiveFileIndex(i())}>
+              <div class="file-color-dot" style={{"background-color": fileHandler.baseColor}}></div>
+              <span class="font-medium">{fileHandler.legSide}</span>
+            </button>
+            <button onClick={() => removeFileSelection(fileHandler.index)}>x</button>
+          </div>
+        )}</For>
+      </div>
+      <p>{activeFile().name} {activeFile().time}</p>
+      <p>Repetitions</p>
+      <ListOfFileHandlerRepetitions fileHandler={activeFile()} />
+      <Button
+        variant="danger"
+        size="sm"
+        onClick={clearSelectedFiles}
+      >
+        Clear all
+      </Button>
+    </Show>
   );
 }
