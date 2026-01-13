d1abcf40694001db76ab623c431624fac2cd42ad
Ilmr1 <ilmarie@metropolia.fi>
2026-01-13 10:04:19 +0200

    fix  base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import solid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: "/isokineettinen-lihasvoimamittaus-2025",
-   plugins: [
+  base: "/MoIn-Isokineettinen-lihasvoimamittaus-2025/",
+  plugins: [
     tailwindcss(),
     solid(),
   ],
